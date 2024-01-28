06317d9534e6458ce886ff68cf92d2c8ba867de7
Ivan Sharapov <oldrum75@gmail.com>
2024-01-28 19:43:26 +0100

    install: react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import axios from 'axios';
 import './App.css';
 import Main from './components/mainPage/Main';
 
 function App() {
-  // const options = {
-  //   method: "GET",
-  //   headers: { "Content-Type": "application/json" },
-  // };
-  // fetch(
-  //   "https://stoplight.io/mocks/kode-frontend-team/koder-stoplight/86566464/users?__example=all",
-  //   options
-  // )
-  //   .then((response) => response.json())
-  //   .then((response) => console.log(response))
-  //   .catch((err) => console.error(err));
+  
+const fetchData = async () => {
+  const url = "https://stoplight.io/mocks/kode-frontend-team/koder-stoplight/86566464/users?__example=all";
+
+  const options = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  };
+
+  try {
+    const response = await axios.request({
+      url,
+      ...options,
+    });
+    
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Call the async function
+fetchData();
 
   return (
     <div className="App">
